5b542de3f28d91e7f33de8d4447cafd11d53ff3b
Frenchy <j.a.french.117@gmail.com>
2017-02-24 13:26:28 -0500

    Add viewlog page table

--- vsearch4web.py
+++ vsearch4web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep=' | ')
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -31,13 +31,19 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(''.join(contents))
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           thetitle='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == '__main__':                           
     app.run(debug=True)
-
-    #git changes test
